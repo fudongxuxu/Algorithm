eb35cb8771ff5155f291592a4c755146b3da79d4
sws <sws@example.com>
2022-06-22 14:17:56 +0800

    1

---
+++ 每日一题/513.找树左下角的值.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=513 lang=javascript
+ *
+ * [513] 找树左下角的值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function (root) {
+  let queue = [];
+  let a = null;
+  queue.push(root);
+  while (queue.length) {
+    let a = queue[0].val;
+    let c = queue.length
+    for (let i = 0; i < c; i++) {
+      let node = queue.shift();
+      if (node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+    }
+  }
+  return a;
+};
+// @lc code=end
+
